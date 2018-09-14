3ae5ffaac2b4ba36c06def591a19ba8a9b3d73aa
Claire Lebastard <claire_lebatard@hotmail.com>
2018-09-14 13:44:18 +0200

    Quete js TDD 1 claire lebastard

--- exercises/id.js
+++ exercises/id.js
@@ -8,6 +8,14 @@
 
 // Your code :
 
+function id(arg){
+	return arg;
+}
+
+function milkshake(lait,fraise){
+	return lait + ' et du ' + fraise
+}
+
 //* Begin of tests
 const assert = require('assert')
 
@@ -15,5 +23,6 @@ assert.strictEqual(typeof id, 'function')
 assert.strictEqual(id.length, 1)
 assert.strictEqual(id(5), 5)
 assert.strictEqual(id('pouet'), 'pouet')
+assert.strictEqual(milkshake('lait de soja','chocolat'), 'lait de soja et du chocolat')
 assert.strictEqual(id(assert), assert)
 // End of tests */
