f7c07a78624d96e6eefc3f9b5383cbb85bd2a7fc
Claire Lebastard <claire_lebatard@hotmail.com>
2018-10-18 14:27:37 +0200

    TDD4 exercices advanced

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -9,11 +9,27 @@
  * Example : "How are you ?" -> "How Are You ?"
  *
  */
+function jadenCase(string) {
+	let newString= '';
+	for (let i=0 ; i<string.length ; i++) {
+		if (string[i-1] === ' ') {
+			let upper = string[i].toUpperCase();
+			newString = newString + upper;
+		}
+		else {
+			newString = newString + string[i];			
+		}
+	}
+	return newString;
+}
+
+console.log(jadenCase('How are you'));
 
 
 
 //* Begin of tests
 const assert = require('assert')
 
-assert.fail('You must write your own tests')
+assert.strictEqual(typeof jadenCase, 'function')
+assert.strictEqual(jadenCase('You must write your own tests'), 'You Must Write Your Own Tests')
 // End of tests */
--- exercises/multiply-recursive.js
+++ exercises/multiply-recursive.js
@@ -11,6 +11,20 @@
 
 // Your code :
 
+function multiply (x, y) {
+	if (y ===0 || x===0) {
+		return 0;	
+	}
+	
+	if (y>0){
+		return x + multiply(x, y-1);
+	}
+	else if (y<0) {
+		return (-x + multiply(x, y+1));
+	}
+
+}
+
 //* Begin of tests
 const assert = require('assert')
 
